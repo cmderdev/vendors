fa34b9a40a1c8c165d3a1c8f3cac4dca476ad202
Martin Kemp <crazykempy92@gmail.com>
2015-03-25 14:46:03 +0000

    Separate the  json files
    the bash script may need a change but should work for now.

--- testjson.sh
+++ testjson.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-for f in *.json; do ./node_modules/.bin/jsonlint -q $f; done
+for f in required/*.json; do ./node_modules/.bin/jsonlint -q $f; done
+for f in optional/*.json; do ./node_modules/.bin/jsonlint -q $f; done
